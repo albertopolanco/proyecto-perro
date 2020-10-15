a9df8cf00210247788ad9f4ed97c6550271db2c8
Alberto <a.p.r.13287@gmail.com>
2020-10-15 10:50:03 +0200

    readme

---
+++ Readme.md
@@ -0,0 +1,51 @@
+<h1>Do you know all dog breeds?</h1>
+
+<p> Through this website you can select a dog breed and a random image of that breed will be printed. See which one is your favorite, there are many to choose from.</p>
+
+
+<h1>Minimum Viable Product</h1>
+
+All pages will have a navigation bar with links to the rest of the sections:
+
+- <h3>Home:</h3> The main page will have a carousel of images, below a brief explanation of how the dog breeds came about and a footer with links to social networks.
+
+- <h3>App</h3> page: In the App page we will have a selector where we will have all the names of the breeds of dogs and when clicking on one it will return a random image of that breed.
+
+- <h3>FAQ:</h3> Here we will have three of the most common questions about dogs.
+
+- <h3>Sign:</h3> up: Here we will have an image and a form to register the page.
+
+- <h3>Login:</h3> There will be an image and a form to log in.
+
+
+
+<h1>Backlog</h1>
+
+- Log in when you register your dog's image if you have one.
+- Include the image to get the characteristics of each chosen breed.
+
+
+<h1>Data Structure</h1>
+
+· root/
+    · index.html
+    · App-page.html
+    · dashboard.html
+    · login.html
+    · signup.html
+    · readme.md
+
+    · css/
+        · style.css
+
+    · scripts/
+        · Database.js
+        · Login.js
+        · Signup.js
+        · User.js
+        · Validator.js
+        · perroApiService.js
+
+    
+    · Imagenes/
+    
--- scripts/perroApiService.js
+++ scripts/perroApiService.js
@@ -1,312 +1,46 @@
-// const razasUrl = 'https://api.thedogapi.com/v1/breeds';
-// const imagenesUrl = 'https://dog.ceo/api/breeds/image';
+const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';//define una propiedad con el valor de la url de la api.
 
-"use strict";
-const formulario = document.querySelector('#formulario')
-formulario.addEventListener('submit', (event)=> {
-  event.preventDefault()
-  const searchBar = document.querySelector('#search_input').value
-  fetchImagesDogs(searchBar)
-})
+const select = document.querySelector('.breeds');//define una propiedad con el elemento con la clase 'breeds' del DOM.
 
-const dogList = document.querySelector('#search_results')
-async function fetchImagesDogs(raza) {
-    const response = await fetch(`https://api.thedogapi.com/v1/breeds/search?name=${raza}`);
-    const dogJson = await response.json(); 
-      const dogId = dogJson.map(async function(dog){
-        const id= dog.id;
-        const getImage = await fetch(`https://api.thedogapi.com/v1/images/search?breed_id=${id}`);
-        const getImageJson = await getImage.json()
-    return {getImageJson}
+fetch(BREEDS_URL)//hace una llamada a la api declada con BREEDS_URL
+  .then(res => {//cuando acabes dehacer la llamada me devuelves res.json
+    return res.json();
+  })
+  .then(data => {//cuando me es devuelto el resultado de la api hazme esto
+    const breedsObject = data.message;//define una propiedad con el objeto message de la respuesta de la api
+    const breedsArray = Object.keys(breedsObject);//define una propiedad de tipo array con todas las keys del objeto breedsObject
+    for (let i = 0; i < breedsArray.length; i++) {//un bucle que recorrera todos los elementos del breedsArray
+      const option = document.createElement('option');// define una propiedad con un elemento nuevo de tipo option
+      option.value = breedsArray[i];//setea el value al elemnto option que acabamos de crear 
+      option.innerText = breedsArray[i];//setea el texto al elemento option que acabamos de crear
+      select.appendChild(option);//añade el elemento opton que acabamos de crear al elemento select definido en la linea 3
+    }
+    console.log(breedsArray);// te pinta en consola el objeto breedsArray
+  });
+
+select.addEventListener('change', event => {//define un escuchador del evento change para el elemnto select definido en la linea 3
+  //este codigo se ejecutara cuando se dispare el evento change del elemento select
+  let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;//define una variable con la url donde ira a buscar las imagenes
+  getDoggo(url);//ejecuta la funcion getDoggo
+});
+
+const img = document.querySelector('.dog-img');//define una propiedad con el elemento con la clase 'dog-img' del DOM
+const spinner = document.querySelector('.spinner');//define una propiedad con elemento con la clase 'spinner' del DOM
+
+const getDoggo = url => {//define la funcion getDoggo el cual espera el parametro url
+  spinner.classList.add('show');//añade la clase'show' al elemnto spinner definido en la linea 27
+  img.classList.remove('show');//elimina la clase 'show' del elemento img definido en la linea 26
+  fetch(url)//hace una llamada a la api declarada con url
+    .then(res => {////cuando acabes dehacer la llamada me devuelves res.json
+      return res.json();
     })
-  return Promise.all(dogId);
-  }
-
-  function generateHtml (data) {
-    data.map(dog=>{
-      const section = document.createElement('section')
-      section.innerHTML = `<img src="${dog.url}">`
-      dogList.appendChild(section)
-    })
-  }
-  fetchImagesDogs('Shiba Inu')
-
-
-
-
-//   //createListItems
-// function createListItems(data){
-//   let output = '';
-//   Object.keys(data).forEach(key => output+=`<option value="${key}">${fixBreed(key)}</option>`);
-//   document.getElementById('doggoDropDown').innerHTML = output;
-// }
-
-
-
-// //GLOBAL VARIABLES
-// const randomButton = document.querySelector('.random');
-// const dogList = document.getElementById('doggoDropDown');
-// const listButton = document.querySelector('.list');
-// const refreshButton = document.querySelector('.grid');
-// //spinner
-
-
-// //EVENT LISTENERS
-// // window load
-// window.addEventListener('load', populateList);
-// window.addEventListener('load', getDogTerm);
-// window.addEventListener('load', createImageGrid);
-// //click and change
-// randomButton.addEventListener('click', getRandomDoggo);
-// dogList.addEventListener('change', getBreedImage);
-// listButton.addEventListener('click', getBreedImage);
-// refreshButton.addEventListener('click', createImageGrid);
-
-// //FETCH CALLS
-// //wikipedia dog term serach
-// function getDogTerm(){
-//   fetch('https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=dog&formatversion=2')
-//     .then(checkStatus)
-//     .then(response => response.json())
-//     .then(data => displayDogInfo(data))
-//     .catch(error => notifyUser(error))
-// }
-
-// //random dog image
-// function getRandomDoggo(){
-//   fetch('https://dog.ceo/api/breeds/image/random')
-//     .then(checkStatus)
-//     .then(response => response.json())
-//     .then(data => handleData(data))
-//     .catch(error => notifyUser(error))
-// }
-
-// //populate List
-// function populateList(){
-//   fetch('https://dog.ceo/api/breeds/list/all')
-//     .then(checkStatus)
-//     .then(response => response.json())
-//     .then(data => createListItems(data.message))
-//     .catch(error => notifyUser(error))
-// }
-
-// //getBreedImage
-// function getBreedImage(){
-//   //get list value
-//   let selectedBreed = dogList.options[dogList.selectedIndex].value;
-//   //build url
-//   let url = `https://dog.ceo/api/breed/${selectedBreed}/images`;
-//   //fetch call
-//   fetch(url)
-//     .then(checkStatus)
-//     .then(response => response.json())
-//     .then(data => getImageURL(data.message))
-//     .catch(error => console.log(error))
-// }
-
-// function createImageGrid(){
-//   fetch('https://dog.ceo/api/breeds/image/random/3')
-//   .then(checkStatus)
-//   .then(response => response.json())
-//   .then(data => createGrid(data.message))
-//   .catch(error => notifyUser(error))
-// }
-
-// //HELPER FUNCTIONS
-// //checkStatus
-// function checkStatus(response){
-//   if(response.ok){
-//     return Promise.resolve(response);
-//   }else{
-//     return Promise.reject(new Error(response.statusText));
-//   }
-// }
-
-// //display Dog Info
-// function displayDogInfo(data){
-//   //populate paragraph
-//   document.getElementById('dogInfo').innerHTML = `<strong>${capitalize(data[0])}:</strong> ${data[2][0]} <a href="${data[3][0]}" alt="read more on wikipedia"> Read more on wikipedia</a>`;
-// }
-
-
-// //handleData
-// function handleData(data){
-//   let url = data.message;
-//   console.log(url)
-//   let regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+\-?\w+)\/.+/g;
-//   let breedName = regex.exec(url);
-//   document.getElementById('randomImageContainer').innerHTML = `<img alt="random image of a ${fixBreed(breedName[1])}" src='${url}'/>`;
-//   document.querySelector('.dogInfo').innerHTML = `<p class="h5">Random image of a ${fixBreed(breedName[1])}</p>`;
-// }
-
-// //getImageURL
-// function getImageURL(data){
-//   //get random number
-//   let randomNumberURL = data[Math.floor(Math.random()*data.length)+1];
-//   listImageContainer.innerHTML = `<img src="${randomNumberURL}" alt="${extractBreedName(data)}"/>`;
-// }
-
-// //createListItems
-// function createListItems(data){
-//   let output = '';
-//   Object.keys(data).forEach(key => output+=`<option value="${key}">${fixBreed(key)}</option>`);
-//   document.getElementById('doggoDropDown').innerHTML = output;
-// }
-
-// //notifyUser
-// function notifyUser(error){
-//   const errorContainer = document.querySelector('.alert');
-//   errorContainer.innerHTML = `There was an error with the server request (${error}). <br> Click the button again.`;
-//   errorContainer.style.display = 'block';
-//   setTimeout(()=>{
-//     errorContainer.innerHTML = '';
-//     errorContainer.style.display ='none';
-//   },4000)
-// }
-
-// //fixBreed
-// function fixBreed(breedName){
-//   if(breedName === 'germanshepherd'){
-//     return 'German Shepherd';
-//   }else if(breedName === 'mexicanhairless'){
-//     return 'Mexican Hairless';
-//   }else if(breedName === 'stbernard'){
-//     return 'St. Bernard';
-//   }else if(breedName === "african"){
-//     return 'African Wild Dog';
-//   }else if(breedName === 'bullterrier'){
-//     return 'Bull Terier';
-//   }
-//   return capitalize(breedName);
-// }
-
-// //capitalize breed name
-// function capitalize(breedName){
-//   return breedName.replace(/\-/g,' ')
-//                   .split(" ")
-//                   .map(word => word.charAt(0).toUpperCase()+word.slice(1))
-// 				          .join(" ");
-// }
-
-// //extract breed name
-// function extractBreedName(data){
-//   let regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+-?\w+)\/\w+\.\w+/g;
-//   let match = regex.exec(data);
-//   return fixBreed(match[1]);
-// }
-
-// //createGrid
-// function createGrid(data){
-//   let output = '';
-//   const container = document.querySelector('.card-deck');
-//   data.map(item =>{
-//     output+=
-//     `
-//     <div class="card mb-4 box-shadow">
-//       <div class="card-header">
-//         <h4 class="my-0 font-weight-normal">${extractBreedName(item)}</h4>
-//       </div>
-//       <div class="card-body">
-//         <img src="${item}" class="img-fluid" alt="${extractBreedName(item)}"/>
-//       </div>
-//     </div>    
-//     `
-//   })
-//     container.innerHTML = output;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use strict';
-
-// function getPokemons () {
-
-//   const section = document.querySelector(".pokemon-list");
-
-
-//   for (let i = 0; i < 21; i++) {
-//     // cada pokemon lleva su propio indice
-//     // tenemos que añadir el indice al fin de URL de la API
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`)
-//       .then((response) => {
-//         // convertiremos la respuesta a un objeto legible,
-//         // porque los datos de la respuesta (response.body) están codificados
-
-//         return response.json(); // response.json() es tambien una operacion asincrona
-//       })
-//       .then((data) => {
-        
-//         const article = document.createElement('article');
-//         article.innerHTML = `
-//           <img src="${data.sprites.front_default}" alt="${data.name}"/>
-//           <h3>${data.name}</h3>
-//         `;
-
-//         section.appendChild(article);
-//       })
-//       .catch((err) => {})
-//   }
-// }
-
-
-// async function getPokemonsAA () {
-//   const section = document.querySelector(".pokemon-list");
-
-//   for (let i = 0; i < 21; i++) {
-//     try {
-//       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`);
-//       const data = await response.json();
-  
-//       const article = document.createElement('article');
-//       article.innerHTML = `
-//         <img src="${data.sprites.front_default}" alt="${data.name}"/>
-//         <h3>${data.name}</h3>
-//       `;
-  
-//       section.appendChild(article);
-//     } catch (err) {
-      
-//     }
-//   }
-// }
-
-// getPokemonsAA();
+    .then(data => {//cuando me es devuelto el resultado de la api hazme esto
+      img.src = data.message;//setea en el atributo src del elemento img definido en la linea 26 el valor de la propiedad mssage de la rspesta de la llamada a la api
+    });
+};
+
+img.addEventListener('load', () => {//define un escuchador del evento load para el elemento img definido en la linea 26 
+  //este codigo se ejecutara cuando se dispare el evento load sobre el elemnto img
+  spinner.classList.remove('show');//elimina la clase'show' del elemento spinner definido en la linea 27
+  img.classList.add('show');//añade la clase show al elemento img definido en la linea 26
+});
